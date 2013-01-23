4d9a9b6f4fa23b576c432d1fd28bdf0df029cd7b
Amelia Schmidt <amelia@godeldesign.com>
2013-01-23 11:07:54 +1100

    theming but has a problem

--- themes/custom/gp_theme/js/scripts.js
+++ themes/custom/gp_theme/js/scripts.js
@@ -1,9 +1,18 @@
 (function($){
   /* $ is mapped to Drupal's version of jQuery in here */
-  
+
   $(function(){
     /* document is ready in here */
-        
+
+    $.each($('.block-titles .node'), function(i, el) {
+      $(el).mouseenter(function(e) {
+        $('.block-frontcontent .node')
+          .stop()
+          .hide()
+          .eq(i)
+          .fadeTo(400, 1);
+      });
+    });
   });
-  
+
 })(jQuery);
